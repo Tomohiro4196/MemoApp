c2a6202b699959205b53a4c3aedeecdf052bd385
Tomohiro4196 <tomo4196.w@gmail.com>
2022-06-02 10:58:25 +0900

    Create Hello props

--- App.jsx
+++ App.jsx
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello bang style={{ fontSize: 16, backgroundColor: 'orange' }}>Tonny</Hello>
+      {/* Viewタグ = HTMLでの"div"のようなもの */}
       <Text>Open up App.js to start working on your app!</Text>
       {/* "eslint-disable-next-line"でStatusBarを無効にしている */}
       {/* eslint-disable-next-line */}
