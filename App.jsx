14f7821985c2d8a2cd34c968e17a970523ee24c1
Tomohiro4196 <tomo4196.w@gmail.com>
2022-06-02 15:15:21 +0900

    style memo screen

--- App.jsx
+++ App.jsx
@@ -1,19 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang>World</Hello>
-      <Hello bang style={{ fontSize: 16, backgroundColor: 'orange' }}>Tonny</Hello>
-      {/* Viewタグ = HTMLでの"div"のようなもの */}
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* "eslint-disable-next-line"でStatusBarを無効にしている */}
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarTitle}>Memo App</Text>
+          <Text style={styles.appBarLogout}>Log put</Text>
+        </View>
+      </View>
+
+      <View>
+
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022/06/02 14:40</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022/06/02 14:40</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022/06/02 14:40</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.addBottom}>
+          <Text style={styles.addBottomLabel}>+</Text>
+      </View>
+
     </View>
   );
 }
@@ -21,8 +55,82 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f4f8',
+  },
+
+  appBar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+
+  appBarInner: {
     alignItems: 'center',
+  },
+
+  appBarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  appBarLogout: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'white',
+  },
+
+  MemoListItem: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 0.5,
+    borderColor: 'rgba(0, 0, 0, 15)',
+  },
+
+  MemoListTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+
+  MemoListDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: 'grey',
+  },
+
+  MemoItemDelete: {
+  },
+
+  addBottom: {
+    backgroundColor: 'skyblue',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+
+  addBottomLabel: {
+    color: 'white',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
